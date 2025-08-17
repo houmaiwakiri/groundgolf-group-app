2e46ad8004a5fcf889ec295d9caaf9d8eecaa882
houmaiwakiri <mahomahoma888@gmail.com>
2025-08-17 17:47:19 +0900

    環境構築用ファイル(未完)

---
+++ work/makeworld.sh
@@ -0,0 +1,5 @@
+echo "プロジェクトを配置したいディレクトリに移動してから実行してください。"
+
+git clone https://github.com/groundgolf-group-app/groundgolf-group-app.git
+cd groundgolf-group-app
+npx create-react-app front --template typescript

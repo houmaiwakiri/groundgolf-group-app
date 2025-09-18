415fb29968dcc54627e8865deb8c1a45b9d65ec4
houmaiwakiri <mahomahoma888@gmail.com>
2025-09-18 22:20:34 +0900

    front-web廃止＆クリーンアーキテクチャ廃止

--- front-app/app/scoreList.tsx
+++
@@ -1,5 +0,0 @@
-import ScoreListScreen from "../src/presentation/screens/ScoreListScreen";
-
-export default function Page() {
-  return <ScoreListScreen />;
-}
--- front-app/src/infrastructure/api/axiosClient.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { API_BASE_URL, DEFAULT_HEADERS } from '../../config';
-
-export const axiosClient = axios.create({
-  baseURL: API_BASE_URL,
-  headers: DEFAULT_HEADERS,
-});

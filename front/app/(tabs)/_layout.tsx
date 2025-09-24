52169bd42f331ba75866dcaba7d5234876a8844a
houmaiwakiri <mahomahoma888@gmail.com>
2025-09-24 22:43:08 +0900

    ログイン機能:認証状態によって表示有無を決める

---
+++ front/app/(tabs)/_layout.tsx
@@ -0,0 +1,64 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: true,
+        tabBarActiveTintColor: "#007AFF",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "ホーム",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="scoreRegister"
+        options={{
+          title: "スコア登録",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="create" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="scoreList"
+        options={{
+          title: "スコア一覧",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="team"
+        options={{
+          title: "チーム",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="other"
+        options={{
+          title: "その他",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="ellipsis-horizontal"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}

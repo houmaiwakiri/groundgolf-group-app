03335e7bd97607ea06ae3fa2eed732d2473863f9
houmaiwakiri <mahomahoma888@gmail.com>
2025-10-18 23:55:15 +0900

    java:エンティティ理解

--- back/src/main/java/com/groundgolfgroupapp/entity/Score.java
+++ back/src/main/java/com/groundgolfgroupapp/entity/Score.java
@@ -8,24 +8,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+// JavaのクラスA:テーブルAのように1対1で対応するJPAの考え方
+
+@Entity // 「このクラスをテーブルとして扱う」という宣言
 public class Score {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // 主キー
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 自動採番
     private Long id;
 
+    // 自動でリストをテーブルとして扱う宣言
+    // score_strokesというテーブルが自動で作成される
     @ElementCollection
     private List<Integer> strokes;
 
-    // エンティティクラスには必ず引数なしのコンストラクタが必要であるため記載
     public Score() {
+        // デフォルトコンストラクタ
+        // JPA(Hibernate)がDBからデータを取り出す時、
+        // リフレクションで new Score() を勝手に呼び出す。
+        // 引数付きしか無いとインスタンスを作れずエラーになる。
     }
 
+    // 引数を自分自身のprivate変数にセットする。
+    // JPAのsave()が呼ぶと、テーブルにオブジェクトの中身を保存する。
     public Score(List<Integer> strokes) {
         this.strokes = strokes;
     }
 
+    // private変数を外から参照するためのメソッド(カプセル化)
     public Long getId() {
         return id;
     }
